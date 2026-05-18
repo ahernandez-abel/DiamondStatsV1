300452fbac36e3339296549e31ff7a5c92cc16fe
Abel Hernandez <abelhernandezestevez@gmail.com>
2026-05-18 10:02:11 -0400

    superadmin

--- frontend/src/api/superadminTenantDetails.api.js
+++ frontend/src/api/superadminTenantDetails.api.js
@@ -4,15 +4,10 @@ export const getSuperadminTenantDetail = async (tenantId) => {
   return await api.get(`/superadmin/tenants/details/${tenantId}`)
 }
 
-export const updateSuperadminTenantPrivacy = async (
-  tenantId,
-  isPublic
-) => {
+export const updateSuperadminTenantPrivacy = async (tenantId, isPublic) => {
   return await api.patch(
     `/superadmin/tenants/details/${tenantId}/privacy`,
-    {
-      is_public: isPublic,
-    }
+    { is_public: isPublic }
   )
 }
 
@@ -33,5 +28,29 @@ export const updateSuperadminTenantStatus = async (
       status,
       suspension_reason: suspensionReason,
     }
+  )
+}
+
+export const updateSuperadminTenantUser = async (tenantId, userId, data) => {
+  return await api.patch(
+    `/superadmin/tenants/details/${tenantId}/users/${userId}`,
+    data
+  )
+}
+
+export const changeSuperadminTenantUserPassword = async (
+  tenantId,
+  userId,
+  password
+) => {
+  return await api.patch(
+    `/superadmin/tenants/details/${tenantId}/users/${userId}/password`,
+    { password }
+  )
+}
+
+export const deleteSuperadminTenantUser = async (tenantId, userId) => {
+  return await api.delete(
+    `/superadmin/tenants/details/${tenantId}/users/${userId}`
   )
 }
